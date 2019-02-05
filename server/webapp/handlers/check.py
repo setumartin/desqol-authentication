7735e5e9163dbda4cca6c05d8d3d1497792de93a
Martin Harrigan <martinharrigan@gmail.com>
2019-02-05 12:08:58 +0000

    Return proper JSON responses for /api/check

--- server/webapp/handlers/check.py
+++ server/webapp/handlers/check.py
@@ -1,20 +1,21 @@
 import datetime
+import time
 import tornado.gen
 
 from .base import BaseHandler
 
-class TokenAuthHandler(BaseHandler):
+class AuthHandler(BaseHandler):
 
     @tornado.gen.coroutine
     def prepare(self):
-        now = mktime(datetime.datetime.now().utctimetuple())
+        super(AuthHandler, self).prepare()
 
+        now = time.mktime(datetime.datetime.now().utctimetuple())
         try:
             token = self.get_argument('token')
         except tornado.web.MissingArgumentError:
             self.current_user = None
-            self.set_status(400)
-            self.finish()
+            self.send_error(400, message='You must provide a token!')
             return
 
         user_dct = yield self.db.users.find_one({'token': token},
@@ -22,18 +23,22 @@ class TokenAuthHandler(BaseHandler):
 
         if not user_dct:
             self.current_user = None
+            self.send_error(403, message='Invalid token!')
             return
 
         if now > user_dct['expires_in']:
             self.current_user = None
+            self.send_error(403, message='Expired token!')
             return
 
         self.current_user = {
             'username': user_dct['username']
         }
 
-class CheckHandler(TokenAuthHandler):
+class CheckHandler(AuthHandler):
 
     @tornado.web.authenticated
     def get(self):
         self.set_status(200)
+        self.response['username'] = self.current_user['username']
+        self.write_json()
